8046ba4dadb4c2c35eddfb34d4ec8e31f1c5aecc
22HOP2D-Chintushig <22HOP2D-Chintushig@Nest-100s-iMac.local>
2022-11-22 19:24:44 +0800

    music

---
+++ app/src/component/HomePage.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "../styles/HomePage.module.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+export const HomePage = () => {
+    return(
+        <div className={styles.Container}>
+            <></>
+            <div className=""></div>
+            <div className=""></div>
+        </div>
+    );
+}
---
+++ app/src/component/Playlists.js
@@ -0,0 +1,19 @@
+export const Playlists = () => {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route></Route>
+            {/* Home */}
+            <Route></Route>
+            {/* Search */}
+            <Route></Route>
+            {/* Library */}
+            <Route></Route>
+            {/* Create Playlist */}
+            <Route></Route>
+            {/* Liked Songs */}
+            <hr></hr>
+        </Routes>
+        </BrowserRouter>
+    );
+}
